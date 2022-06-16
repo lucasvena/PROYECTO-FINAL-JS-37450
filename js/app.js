bc825b6731a4e22e7ad45f864d26998be6849a5c
LucasVena <lsvena@hotmail.com>
2022-06-15 23:06:38 -0300

    1ra Entrega Proyecto Final

--- js/app.js
+++ js/app.js
@@ -26,7 +26,8 @@ function numeroCuotas(precio,iva){
     }
 }
 
-/* LOGGING */
+// LOGGING CLIENTE
+
 let usuario = prompt("Ingrese su nombre:").toUpperCase();
 
 const precioPantalon = 5000;
@@ -39,7 +40,8 @@ while (true) {
     if (usuario === "LUCAS"){
         saludar();
         console.log("Nombre de Perfil: " + usuario);
-        /* INSTANCIA DE COMPRA */
+        
+        // INSTANCIA DE COMPRA
 
         let opciones = prompt ("Ingresa el número correspondiente de lo que quieres comprar: \n 1-Pantalón \n 2-Camisa \n 3-Remera \n 4-Zapatos \n ESC-Salir").toUpperCase();
 
@@ -84,37 +86,67 @@ while (true) {
 
 
     } else if (usuario === "ADMIN") {
-        // ADMIN
+        // AREA ADMIN.
         saludar();
         console.log("Nombre de Perfil: " + usuario);
         alert ("IMPORTANTE: Para el ingreso de productos será necesario completar todos los datos solicitados")
+        console.log("Catálogo existente:")
         
+
+        //CATÁLOGO
+
         class Producto {
-            constructor(id, tipo, precio, stock ) {
+            constructor(id, tipo, color, precio, stock ) {
                 this.id = id
                 this.tipo = tipo
+                this.color = color
                 this.precio = precio
                 this.stock = stock
             }
         }
 
         const catalogo = [
-            new Producto(1, 'Jean Azul', 5000, 20),
-            new Producto(2, 'Jean Negro', 5200, 30),
-            new Producto(3, 'Camisa', 4500, 10),
-            new Producto(4, 'Remera Roja', 3000, 35),
-            new Producto(5, 'Remera Blanca', 3200, 15),
+            new Producto(1, 'Jean', 'Azul', 5000, 10),
+            new Producto(1, 'Jean', 'Celeste', 5000, 10),
+            new Producto(1, 'Jean', 'Negro', 5200, 5),
+            new Producto(1, 'Jean', 'Gris', 5200, 5),
+            new Producto(2, 'Camisa', 'Rayada', 4500, 10),
+            new Producto(2, 'Camisa', 'Lisa', 4500, 10),
+            new Producto(3, 'Remera', 'Roja', 3000, 35),
+            new Producto(3, 'Remera', 'Negra', 3200, 50),
+            new Producto(3, 'Remera', 'Azul', 3000, 35),
+            new Producto(3, 'Remera', 'Estampada', 3500, 20),
+            new Producto(3, 'Remera', 'Blanca', 3200, 50),
+            new Producto(3, 'Remera', 'Estampada', 3800, 10),
         ]
-        console.log(catalogo);
+
+        catalogo.forEach ( (prod) => {
+            console.log (prod);
+        });
+
+
+        // FILTRADO POR TIPO Y PRECIO
+
+        const filtroProd = catalogo.filter ( (prod) => prod.tipo === "Remera" && prod.precio <= 3200)
+        console.log (filtroProd);
+
+        // BUSQUEDA EN STOCK
+
+        const existe = catalogo.some ( (prod) => prod.tipo === "Jean" && prod.color === "Azul")
+        console.log (existe);
 
         while (true){
             function agregarProducto() {
                 let id = Number( prompt ('Ingrese ID del producto') )
                 let tipo = prompt ('Ingrese el tipo de producto')
+                let color = prompt ('Ingrese el color del producto')
                 let precio = Number( prompt ('Ingrese precio del producto') )
                 let stock = Number(prompt ('Ingrese el stock del producto') )
     
-                catalogo.push( new Producto(id, tipo, precio, stock) )
+                catalogo.push( new Producto(id, tipo, color, precio, stock) )
+                catalogo.forEach ( (prod) => {
+                    console.log (prod);
+                });
                 alert ("Se agregó exitosamente!");
             }
 
@@ -123,7 +155,6 @@ while (true) {
             if (carga === "SI") {
                 
                 agregarProducto();
-                // console.log(caramelos);
                             
             } else {
                 alert ("Carga de productos finalizada");
@@ -139,3 +170,5 @@ while (true) {
            
     usuario = prompt ("Ingrese tu nombre").toUpperCase();
 }
+
+
